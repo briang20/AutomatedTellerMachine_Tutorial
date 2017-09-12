9f2354ac511650f1e06c21ffdfb7034416d36607
Brian Getz <briang846@gmail.com>
2017-09-11 20:33:21 -0400

    Initial

--- AutomatedTellerMachine/App_Start/RouteConfig.cs
+++ AutomatedTellerMachine/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace AutomatedTellerMachine
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+                name: "Serial number",
+                url: "serial",
+                defaults: new { controller = "Home", action = "Serial" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
---
+++ AutomatedTellerMachine/MyLoggingFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+
+namespace AutomatedTellerMachine
+{
+    public class MyLoggingFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var request = filterContext.HttpContext.Request;
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
